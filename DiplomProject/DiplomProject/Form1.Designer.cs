81387ab92535a11bee6734e0287818d8448e9c6c
Dmitraud <60182202+Dmitraud@users.noreply.github.com>
2020-01-22 16:01:28 +0300

    Add files via upload

---
+++ DiplomProject/DiplomProject/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace DiplomProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NumBlankCount = new System.Windows.Forms.NumericUpDown();
+            this.BtnCompute = new System.Windows.Forms.Button();
+            this.TableBlankParam = new System.Windows.Forms.DataGridView();
+            this.BlankLength = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BlankWidth = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BlankSquare = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnDBOpen = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.NumBlankCount)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TableBlankParam)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // NumBlankCount
+            // 
+            this.NumBlankCount.Location = new System.Drawing.Point(153, 249);
+            this.NumBlankCount.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.NumBlankCount.Name = "NumBlankCount";
+            this.NumBlankCount.Size = new System.Drawing.Size(51, 20);
+            this.NumBlankCount.TabIndex = 1;
+            this.NumBlankCount.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.NumBlankCount.ValueChanged += new System.EventHandler(this.NumBlankCount_ValueChanged);
+            // 
+            // BtnCompute
+            // 
+            this.BtnCompute.Location = new System.Drawing.Point(12, 292);
+            this.BtnCompute.Name = "BtnCompute";
+            this.BtnCompute.Size = new System.Drawing.Size(75, 23);
+            this.BtnCompute.TabIndex = 2;
+            this.BtnCompute.Text = "Вычислить";
+            this.BtnCompute.UseVisualStyleBackColor = true;
+            this.BtnCompute.Click += new System.EventHandler(this.BtnCompute_Click);
+            // 
+            // TableBlankParam
+            // 
+            this.TableBlankParam.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.TableBlankParam.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.BlankLength,
+            this.BlankWidth,
+            this.BlankSquare});
+            this.TableBlankParam.Location = new System.Drawing.Point(12, 27);
+            this.TableBlankParam.Name = "TableBlankParam";
+            this.TableBlankParam.Size = new System.Drawing.Size(325, 216);
+            this.TableBlankParam.TabIndex = 3;
+            this.TableBlankParam.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.TableBlankParam_CellValueChanged);
+            // 
+            // BlankLength
+            // 
+            this.BlankLength.HeaderText = "Длина заготовки (мм)";
+            this.BlankLength.Name = "BlankLength";
+            this.BlankLength.Width = 90;
+            // 
+            // BlankWidth
+            // 
+            this.BlankWidth.HeaderText = "Ширина заготовки (мм)";
+            this.BlankWidth.Name = "BlankWidth";
+            this.BlankWidth.Width = 90;
+            // 
+            // BlankSquare
+            // 
+            this.BlankSquare.HeaderText = "Площадь заготовки (кв.м)";
+            this.BlankSquare.Name = "BlankSquare";
+            this.BlankSquare.ReadOnly = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "txt";
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnDBOpen
+            // 
+            this.btnDBOpen.Location = new System.Drawing.Point(12, 249);
+            this.btnDBOpen.Name = "btnDBOpen";
+            this.btnDBOpen.Size = new System.Drawing.Size(104, 37);
+            this.btnDBOpen.TabIndex = 4;
+            this.btnDBOpen.Text = "Открыть базу данных";
+            this.btnDBOpen.UseVisualStyleBackColor = true;
+            this.btnDBOpen.Click += new System.EventHandler(this.btnDBOpen_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 15);
+            this.label1.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(358, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(350, 350);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(756, 393);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDBOpen);
+            this.Controls.Add(this.TableBlankParam);
+            this.Controls.Add(this.BtnCompute);
+            this.Controls.Add(this.NumBlankCount);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.NumBlankCount)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TableBlankParam)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NumericUpDown NumBlankCount;
+        private System.Windows.Forms.Button BtnCompute;
+        private System.Windows.Forms.DataGridView TableBlankParam;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnDBOpen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BlankLength;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BlankWidth;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BlankSquare;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ DiplomProject/DiplomProject/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DiplomProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TlpBlanksParam_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void BtnCompute_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
+            int n = TableBlankParam.RowCount; //Количество строк в таблице
+
+            try
+            {
+                int[,] LengthWidthArray = new int[n, 2]; //Массив длин и ширин заготовок
+                int SpaceForDrawWidth = this.Width - TableBlankParam.Width;//Свободное место на форме для рисования
+                pictureBox1.Width = SpaceForDrawWidth;
+                int x = 0, y = 0;
+                int maxY = 0;
+                Graphics g = pictureBox1.CreateGraphics();
+                for (int i = 0; i < n - 1; i++)
+                {
+                    LengthWidthArray[i, 0] = Convert.ToInt16(TableBlankParam[0, i].Value);
+                    LengthWidthArray[i, 1] = Convert.ToInt16(TableBlankParam[1, i].Value);
+                    if (LengthWidthArray[i, 1] > maxY) maxY = LengthWidthArray[i, 1];
+                    MessageBox.Show(Convert.ToString(LengthWidthArray[i, 0]) + "  " + Convert.ToString(LengthWidthArray[i, 1]));
+                    g.DrawRectangle(Pens.Blue, new Rectangle(x, y, LengthWidthArray[i, 0], LengthWidthArray[i, 1]));
+                    x += LengthWidthArray[i, 0] + 2;
+
+                    if (x > 350)
+                    {
+                        x = 0;
+                        y += maxY + 2;
+                        maxY = 0;
+                    }
+
+                }
+            }
+            catch(System.FormatException ex)
+            {
+                MessageBox.Show("Таблица содержит недопустимые символы", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+
+        }
+
+        private void NumBlankCount_ValueChanged(object sender, EventArgs e)
+        {
+          //  TableBlankParam.RowCount= (int)NumBlankCount.Value;
+           // int i = (int)NumBlankCount.Value-1;
+          //  TableBlankParam[0, i].Value = i+1;
+          //  i++;
+
+        }
+
+
+        private void btnDBOpen_Click(object sender, EventArgs e)
+        {
+            
+            
+                openFileDialog1.DefaultExt = ".txt";
+                openFileDialog1.ShowDialog();
+            
+                BindingSource bs;
+                OleDbDataAdapter adapter;
+                DataTable dt;
+
+
+                string DBName = @openFileDialog1.FileName;
+                label1.Text = "Выбрана база " + DBName;
+                string CmdText = "SELECT * FROM [Таблица1]";
+
+                string ConnString = @"Driver={Microsoft Access Driver (*.mdb)}; DBQ=DBName";
+
+                string conBD = string.Format("Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0}", DBName);
+
+                OleDbConnection connection = new OleDbConnection(conBD);
+            try
+            {
+                
+                connection.Open();
+                TableBlankParam.Columns.Clear();
+                string query1 = "SELECT * FROM Таблица1";
+                OleDbCommand cmd1 = new OleDbCommand(query1, connection);
+
+                dt = new DataTable();
+
+                adapter = new OleDbDataAdapter(cmd1);
+                OleDbCommandBuilder AccessCommandBuilder = new OleDbCommandBuilder(adapter);
+                adapter.Fill(dt);
+                dt.Columns.Remove("Код"); //Удаление лишнего столбца после импорта БД
+
+                TableBlankParam.DataSource = dt;
+                BtnCompute.Enabled = true;
+            }
+
+            catch (System.Data.OleDb.OleDbException ex)
+            {
+                MessageBox.Show("База данных не выбрана", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                label1.Text = "";
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        { 
+            openFileDialog1.Filter = "Текстовые файлы (*.txt)|*.txt|Файлы базы данных Access (*.accdb)|*.accdb|Файлы базы данных Access 2002-2003 (*.mdb)|*.mdb";
+            this.MinimumSize = new Size (770, 430);
+            BtnCompute.Enabled = false;
+            
+        }
+
+
+       
+        private void Form1_Resize(object sender, System.EventArgs e)
+        {
+
+        }
+
+
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            e.Graphics.DrawRectangle(Pens.Blue, new Rectangle(10, 10, 100, 100));
+        }
+
+        private void TableBlankParam_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (TableBlankParam[0, 0].Value !=null && TableBlankParam[1, 0].Value != null) BtnCompute.Enabled = true;
+            else BtnCompute.Enabled = false;
+            
+        }
+    }
+}
